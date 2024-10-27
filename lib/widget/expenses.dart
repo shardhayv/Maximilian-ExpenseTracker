96f6f4687b40846dd0ee29d369eb660f3e387af1
shardhayv <shardhayvatshyayan7@gmail.com>
2024-10-27 09:54:14 +0545

    displaying the data in card view

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -1,4 +1,4 @@
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widget/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expenses_model.dart';
 import 'package:flutter/material.dart';
 // import 'package:expense_tracker/models/expenses.dart';
---
+++ lib/widget/expenses_list/expense_item.dart
@@ -0,0 +1,42 @@
+import 'package:expense_tracker/models/expenses_model.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key, required this.expense});
+
+  final ExpensesModel expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 14,
+        ),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                // converting double to string and rounding off the value to only 2 after decimal
+                Text('Rs ${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    const SizedBox(width: 8),
+                    Text(
+                      expense.date.toString(),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
